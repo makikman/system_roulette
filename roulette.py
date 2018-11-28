f3e714cf12f4736a1e0f505e6333a6ed4c9c1330
skids <kettu.posti@outlook.com>
2018-11-28 20:25:28 +0200

    bettering

--- roulette.py
+++ roulette.py
@@ -4,47 +4,49 @@ import os
 import subprocess
 import random
 #open a file that keeps track of trigger pulls
-filename = "shots.txt"
-fd = open(filename, "r")
+FILENAME = "shots.txt"
+FD = open(FILENAME, "r")
 
-#function that initialises the file to 0 shots
 def init_counter(fd):
+    """function that initialises the file to 0 SHOTS"""
     fd.close()
-    fd = open(filename, "w")
+    fd = open(FILENAME, "w")
     fd.write("0")
     fd.close()
 
 #if file is empty initialise it
-if os.stat(filename).st_size == 0:
+if os.stat(FILENAME).st_size == 0:
     print("file was empty. initializing...")
-    init_counter(fd)
+    init_counter(FD)
     exit()
 
 #get the value from file
-shots = int(fd.read())
-print("Trigger has been pressed " + str(shots) + " times.")
+SHOTS = int(FD.read())
+print("Trigger has been pressed " + str(SHOTS) + " times.")
 
 #play russian roulette. if the chamber is loaded
 #your system is deleted
-chance_of_bullet = 1 / (6 - shots)
-if random.random() < chance_of_bullet:
-     init_counter(fd)
-     print("the chamber was loaded")
-    #p = subprocess.Popen(["sudo", "rm", "-r", "-f", "*"], cwd=r'"\", stdout=subprocess.PIPE, shell=True)
-    #while True:
-#        out = p.stdout.read(1)
-#        if out == "" and p.poll() != None:
-#            break
-#        if out != " ":
-#            sys.stdout.write(out)
-#            sys.stdout.flush()
+def roulette(shots, fd):
+    chance_of_bullet = 1 / (6 - shots)
+    if random.random() < chance_of_bullet:
+        init_counter(fd)
+        print("the chamber was loaded")
+    else:
+        shots = shots + 1
+        fd.close()
+        fd = open(FILENAME, "w")
+        fd.write(str(shots))
+        fd.close()
 
-#if the chamber was not loaded
-#keep track of shots so the chance increases
-#shots are stored in file
-else:
-    shots = shots + 1
-    fd.close()
-    fd = open(filename, "w")
-    fd.write(str(shots))
-    fd.close()
+
+        """
+        p = subprocess.Popen(["sudo", "rm", "-r", "-f", "*"], cwd=r'"\", stdout=subprocess.PIPE, shell=True)
+        while True:
+             out = p.stdout.read(1)
+             if out == "" and p.poll() != None:
+             break
+             if out != " ":
+                sys.stdout.write(out)
+                sys.stdout.flush()
+                """
+roulette(SHOTS, FD)
